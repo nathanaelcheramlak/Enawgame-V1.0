09a63fce21e893947e0b780ad456a39ca1dfefb3
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-10-24 23:35:07 +0300

    added new route /username/:username

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import protectRoute from '../middleware/protectRoute.js';
-import { getUsers } from '../controllers/user.controller.js';
+import {
+  getUsers,
+  getUserById,
+  getUserByUsername,
+} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/', protectRoute, getUsers);
+router.get('/:id', protectRoute, getUserById);
+router.get('/username/:username', protectRoute, getUserByUsername);
 
 export default router;
